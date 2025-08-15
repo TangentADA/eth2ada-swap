e62d51d5601d478c0e223f460f308e0de34c97cd
Gavin Karlsson <100855173+GavinKarlsson@users.noreply.github.com>
2025-08-16 04:58:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
 import dynamic from 'next/dynamic';
 import "../styles/globals.css";
-import "../styles/swap.scss";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/layout";
 import { Provider } from "react-redux";
-import { store } from "../store"; // Adjust to ../redux/store if needed
+import { store } from "../store";
 import { useRouter } from "next/router";
-import { MetaMaskProvider } from "metamask-react";
 import Meta from "../components/Meta";
-
-// Dynamically import ClientWrapper with SSR disabled
-const ClientWrapper = dynamic(() => import('../components/ClientWrapper'), {
-  ssr: false, // Disable SSR for this component
-});
+const ClientWrapper = dynamic(() => import('../components/ClientWrapper'), { ssr: false });
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -23,17 +17,15 @@ function MyApp({ Component, pageProps }) {
       <Meta title="Eth2Ada Dictionary" />
       <Provider store={store}>
         <ThemeProvider enableSystem={true} attribute="class">
-          <MetaMaskProvider>
-            <ClientWrapper>
-              {pid === "/login" ? (
+          <ClientWrapper>
+            {pid === "/login" ? (
+              <Component {...pageProps} />
+            ) : (
+              <Layout>
                 <Component {...pageProps} />
-              ) : (
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              )}
-            </ClientWrapper>
-          </MetaMaskProvider>
+              </Layout>
+            )}
+          </ClientWrapper>
         </ThemeProvider>
       </Provider>
     </>
