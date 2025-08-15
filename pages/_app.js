eaf1cb2343c31d9120158679f0e1c26157ee8b67
Gavin Karlsson <100855173+GavinKarlsson@users.noreply.github.com>
2025-08-16 03:10:54 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/layout";
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import { store } from "../store"; // Updated from ../redux/store
 import { useRouter } from "next/router";
 import { MetaMaskProvider } from "metamask-react";
 import Meta from "../components/Meta";
 import UserContext from "../components/UserContext";
 import { useEffect, useRef } from "react";
+import 'react-toastify/dist/ReactToastify.css';
+import '@dexhunterio/swaps/lib/assets/style.css';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -16,11 +18,9 @@ function MyApp({ Component, pageProps }) {
     scrollPos: 0,
   });
 
-
   return (
     <>
       <Meta title="Eth2Ada Dictionary" />
-
       <Provider store={store}>
         <ThemeProvider enableSystem={true} attribute="class">
           <MetaMaskProvider>
